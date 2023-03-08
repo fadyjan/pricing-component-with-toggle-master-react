d2e1bc309680e65abce916ecaaaa9129c13e786f
fadyjan <fady.jan95@gmail.com>
2023-03-08 02:05:05 +0200

    Create #3 Final touches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, createContext } from "react";
+import "./App.css";
+import Card from "./components/card/card";
+import ToggleSwitch from "./components/ToggleSwitch/ToggleSwitch";
+export const PricePlan = createContext("Monthly");
 
 function App() {
+
+  const [pricePlan, setPricePlan] = useState("Monthly")
+
+  const PriceHanler = (Currentstate)=>{
+    setPricePlan(Currentstate)
+  }
+  const Data = [
+    {
+      PlanName: "Basic",
+      Price: {"Monthly":"19.99" , "Annually":"199.99"},
+      Storage: "500 GB",
+      UserAllowed: "2",
+      SendUp: "3 GB",
+    },
+    {
+      PlanName: "Professional",
+      Price:{"Monthly":"24.99" , "Annually":"249.99"} ,
+      Storage: "1 TB",
+      UserAllowed: "5",
+      SendUp: "10 GB",
+    },
+    {
+      PlanName: "Master",
+      Price:{"Monthly":"39.99" , "Annually":"399.99"} ,
+      Storage: "2 TB",
+      UserAllowed: "10",
+      SendUp: "20 GB",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PricePlan.Provider value={{pricePlan , PriceHanler}}>
+      <div className="App">
+        <h1>Our Pricing</h1>
+        <ToggleSwitch  ></ToggleSwitch>
+        <div id="MainContainer">
+          {Data.map((record) => {
+            return <Card record={record}></Card>;
+          })}
+        </div>
+      </div>
+    </PricePlan.Provider>
   );
 }
 
